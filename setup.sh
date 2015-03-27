c1210308cea60ee0ef43102f18098e11c6613369
ItsEddy <itseddy0402@gmail.com>
2015-03-28 00:18:22 +0800

    Add zsh and some missing vim configuration

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "${0%/*}"
 
-# Vim
+# VIM
 if [ ! -f ~/.vimrc ]; then
     echo Symlink `pwd`/vim/vimrc to ~/.vimrc
     ln -s `pwd`/vim/vimrc ~/.vimrc
@@ -14,4 +14,23 @@ if ! diff --brief `pwd`/vim/clang_completer.py vim/vim/bundle/YouCompleteMe/thir
 then
     echo Patch YoucompleteMe
     cp `pwd`/vim/clang_completer.py vim/vim/bundle/YouCompleteMe/third_party/ycmd/ycmd/completers/cpp/clang_completer.py
+fi
+if [ ! -f ~/.ycm_extra_conf.py ]; then
+    echo Symlink `pwd`/vim/ycm_extra_conf.py to ~/.ycm_extra_conf.py
+    ln -s `pwd`/vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
+fi
+if [ ! -f ~/.NERDTreeBookmarks ]; then
+    echo Symlink `pwd`/vim/NERDTreeBookmarks to ~/.NERDTreeBookmarks
+    ln -s `pwd`/vim/NERDTreeBookmarks ~/.NERDTreeBookmarks
+fi
+
+
+# ZSH
+if [ ! -f ~/.zshrc ]; then
+    echo Symlink `pwd`/zsh/zshrc to ~/.zshrc
+    ln -s `pwd`/zsh/zshrc ~/.zshrc``
+fi
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo Symlink `pwd`/zsh/oh-my-zsh to ~/.oh-my-zsh
+    ln -s `pwd`/zsh/oh-my-zsh ~/.oh-my-zsh
 fi
